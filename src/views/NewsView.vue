8698ae0a4b202e01c28ac4866b7a82e599beb36f
ykchoi99 <choe_gang@naver.com>
2022-10-25 22:17:40 +0900

    라우터 및 api 설정

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>news</div>
+<div>
+  <li v-for="user in users" v-bind:key="user.id">
+    {{user}}
+  </li>
+</div>
 </template>
 
 <script>
+
+import {fetchNewsList} from '../api/index.js';
+
 export default {
+ data(){
+  return {
+    users:[],
+  }
+ },
+
+ created(){
+  fetchNewsList()
+  .then(response => {
+      this.users = response.data;
+    })
+  .catch(error => console.log(error))
+ }
 
 }
+
 </script>
 
 <style>
