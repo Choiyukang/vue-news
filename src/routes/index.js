16a50b86055e4b74151c84d733e8fb60fcfd87a7
ykchoi <choe_gang@naver.com>
2022-11-07 15:36:06 +0900

    mixin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-// import NewsView from "../views/NewsView.vue";
-// import AskView from "../views/AskView.vue";
-// import JobsView from "../views/JobsView.vue";
+import NewsView from "../views/NewsView.vue";
+import AskView from "../views/AskView.vue";
+import JobsView from "../views/JobsView.vue";
 import ItemView from "../views/ItemView.vue";
 import UserView from "../views/UserView.vue";
 import CreateListView from "../views/CreateListView.js"
@@ -19,18 +19,20 @@ export const router = new VueRouter({
     {
       path: "/news", //url 주소
       name: "news",
-      // component: NewsView, // url 주소로 갔을 때 표시될 컴포넌트
-      component: CreateListView('NewsView'),
+      component: NewsView, // url 주소로 갔을 때 표시될 컴포넌트
+      // component: CreateListView('NewsView'),
     },
     {
       path: "/ask",
       name: "ask",
-      component: CreateListView('AskView'),
+      component: AskView, // url 주소로 갔을 때 표시될 컴포넌트
+      // component: CreateListView("AskView"),
     },
     {
       path: "/jobs",
       name: "jobs",
-      component: CreateListView('JobsView'),
+      component: JobsView, // url 주소로 갔을 때 표시될 컴포넌트
+      // component: CreateListView("JobsView"),
     },
     {
       path: "/user/:id",
