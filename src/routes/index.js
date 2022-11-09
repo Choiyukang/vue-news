4a767555679bbbd3a4b5c85b70b6d043b92fa6f7
ykchoi <choe_gang@naver.com>
2022-11-09 11:45:39 +0900

    ....

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ import JobsView from "../views/JobsView.vue";
 import ItemView from "../views/ItemView.vue";
 import UserView from "../views/UserView.vue";
 import CreateListView from "../views/CreateListView.js"
+import bus from "@/utils/bus";
+import { store } from "@/store";
 
 Vue.use(VueRouter);
 
@@ -20,6 +22,20 @@ export const router = new VueRouter({
       path: "/news", //url 주소
       name: "news",
       component: NewsView, // url 주소로 갔을 때 표시될 컴포넌트
+      beforeEnter : (to, from, next) =>{
+        // console.log('to',to) // 이동할 url라우팅정보
+        // console.log('from',from) //현재위치 라우팅정보
+        bus.$emit("start:spinner");
+        store.dispatch("FETCH_LIST", to.name)
+          .then(() => {
+            bus.$emit("end:spinner");
+            next();
+          })  
+          .catch(() => {
+            bus.$emit("end:spinner");
+          });
+        //next(); //호출 안하면 이동 안함
+      }
       // component: CreateListView('NewsView'),
     },
     {
--- src/store/actions.js
+++ src/store/actions.js
@@ -9,7 +9,7 @@ import {
 
 export default{
     FETCH_NEWS(context){
-            fetchNewsList()
+           return fetchNewsList()
             .then(response => {
                 context.commit('SET_NEWS',response.data)
                 return response
@@ -17,36 +17,42 @@ export default{
             .catch(error => console.log(error))
         },
         FETCH_ASK({commit}){
-            fetchAskList()
-            .then(({data}) => commit('SET_ASK',data))
-            .catch(error => console.log(error))
+            return fetchAskList()
+              .then(({ data }) => commit("SET_ASK", data))
+              .catch((error) => console.log(error));
         },
         FETCH_JOBS({commit}){
-            fetchJobsList()
-            .then(({data}) => {
-                commit('SET_JOBS',data)
-            })
-            .catch(error => console.log(error))
+            return fetchJobsList()
+              .then(({ data }) => {
+                commit("SET_JOBS", data);
+              })
+              .catch((error) => console.log(error));
         },
         FETCH_USER({commit}, name){
-            fetchUserInfo(name)
-            .then(({data})=>{
-                commit('SET_USER', data)
-            })
-            .catch(error => {console.log(error)})
+            return fetchUserInfo(name)
+              .then(({ data }) => {
+                commit("SET_USER", data);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
         },
         FETCH_ITEM({commit}, itemId){
-            fetchCommentItem(itemId)
-            .then(({data})=>{
-                commit('SET_ITEM', data)
-            })
-            .catch(error => {console.log(error)})
+            return fetchCommentItem(itemId)
+              .then(({ data }) => {
+                commit("SET_ITEM", data);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
         },
         FETCH_LIST({commit}, pageName){
-            fetchList(pageName)
-            .then(({data})=>{
-                commit('SET_LIST', data)
-            })
-            .catch(error => {console.log(error)})
+            return fetchList(pageName)
+              .then(({ data }) => {
+                commit("SET_LIST", data);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
         },
 }
